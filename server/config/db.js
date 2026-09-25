182f0444613123a9143244e36664cd40d4a4d6d5
Leegan S <161812234+Leegan8484@users.noreply.github.com>
2026-09-25 07:40:31 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,23 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
-// Create MySQL Connection
+console.log("Host:", process.env.DB_HOST);
+console.log("User:", process.env.DB_USER);
+console.log("Database:", process.env.DB_NAME);
+console.log("Port:", process.env.DB_PORT);
+
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: Number(process.env.DB_PORT),
+    connectTimeout: 30000
 });
 
-// Connect to MySQL
 db.connect((err) => {
     if (err) {
-        console.error("❌ Database Connection Failed");
+        console.error("Database Error:");
         console.error(err);
         return;
     }
